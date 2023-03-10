9edbed811ad91f82f62a056acb4df42f2851d1cc
xyznodes.eth <120067577+erdinin@users.noreply.github.com>
2023-03-11 00:41:52 +0300

    Update common.sh

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@ CYAN="\e[1m\e[1;96m" # cyan color
 RED="\e[1m\e[1;91m" # red color
 
 function printLogo {
-  bash <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/logo.sh)
+  bash <(curl -s https://raw.githubusercontent.com/erdinin/testnet-guides/main/logo.sh?token=GHSAT0AAAAAAB6XXJ6HWNPNG3WABNO4QTYIZALUQSQ)
 }
 
 function printLine {
