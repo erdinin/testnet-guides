a6167c7be723b8abb92fbfba3e9f8757b089d292
xyznodes.eth <120067577+erdinin@users.noreply.github.com>
2023-03-27 05:14:37 +0300

    Update dependencies.sh

--- utils/dependencies.sh
+++ utils/dependencies.sh
@@ -2,13 +2,13 @@
 
 source <(curl -s https://raw.githubusercontent.com/erdinin/testnet-guides/main/utils/common.sh)
 
-printCyan "1. Updating packages..." && sleep 1
+printCyan "1. Güncellemeler yapılıyor..." && sleep 1
 sudo apt update
 
-printCyan "2. Installing dependencies..." && sleep 1
+printCyan "2. Gereklilikler kuruluyor..." && sleep 1
 sudo apt install -y make gcc jq curl git lz4 build-essential chrony unzip
 
-printCyan "3. Installing go..." && sleep 1
+printCyan "3. Go yükleniyor..." && sleep 1
 if ! [ -x "$(command -v go)" ]; then
   source <(curl -s "https://raw.githubusercontent.com/erdinin/testnet-guides/main/utils/go-install.sh")
   source .bash_profile
