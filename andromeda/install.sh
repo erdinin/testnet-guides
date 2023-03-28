eb145087dd004ef4d6b1358ae531dae235c779d8
xyznodes.eth <120067577+erdinin@users.noreply.github.com>
2023-03-29 02:02:33 +0300

    Create install.sh

---
+++ andromeda/install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/erdinin/testnet-guides/main/utils/common.sh)
+
+printLogo
+
+read -r -p "Enter node moniker: " NODE_MONIKER
+
+CHAIN_ID="galileo-3"
+CHAIN_DENOM="uandr"
+BINARY_NAME="andromedad"
+BINARY_VERSION_TAG="galileo-3-v1.1.0-beta1"
+
+printLine
+echo -e "Node moniker:       ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:           ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:        ${CYAN}$CHAIN_DENOM${NC}"
+echo -e "Binary version tag: ${CYAN}$BINARY_VERSION_TAG${NC}"
+printLine
+sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/erdinin/testnet-guides/main/utils/dependencies.sh)
+
+printCyan "4. Binary yapılandırılıyor..." && sleep 1
+
+cd || return
+rm -rf andromedad
+git clone https://github.com/andromedaprotocol/andromedad.git
+cd andromedad || return
+git checkout galileo-3-v1.1.0-beta1
+make install
+andromedad version # galileo-3-v1.1.0-beta1
+
+andromedad config keyring-backend test
+andromedad config chain-id $CHAIN_ID
+andromedad init "$NODE_MONIKER" --chain-id $CHAIN_ID
+
+curl -s https://raw.githubusercontent.com/andromedaprotocol/testnets/galileo-3/genesis.json > $HOME/.andromedad/config/genesis.json
+curl -s https://snapshots2-testnet.nodejumper.io/andromeda-testnet/addrbook.json > $HOME/.andromedad/config/addrbook.json
+
+SEEDS=""
+PEERS=""
+sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.andromedad/config/config.toml
+
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.andromedad/config/app.toml
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.andromedad/config/app.toml
+sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $HOME/.andromedad/config/app.toml
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 0|g' $HOME/.andromedad/config/app.toml
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001uandr"|g' $HOME/.andromedad/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.andromedad/config/config.toml
+
+printCyan "5. Servis ve senkronizasyon başlatılıyor..." && sleep 1
+
+sudo tee /etc/systemd/system/andromedad.service > /dev/null << EOF
+[Unit]
+Description=Andromeda testnet Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which andromedad) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+andromedad tendermint unsafe-reset-all --home $HOME/.andromedad --keep-addr-book
+
+SNAP_NAME=$(curl -s https://snapshots2-testnet.nodejumper.io/andromeda-testnet/info.json | jq -r .fileName)
+curl "https://snapshots2-testnet.nodejumper.io/andromeda-testnet/${SNAP_NAME}" | lz4 -dc - | tar -xf - -C "$HOME/.andromedad"
+
+sudo systemctl daemon-reload
+sudo systemctl enable andromedad
+sudo systemctl start andromedad
+
+printLine
+echo -e "Log kontrolü:            ${CYAN}sudo journalctl -u $BINARY_NAME -f --no-hostname -o cat ${NC}"
+echo -e "Senkronizasyon durumu: ${CYAN}$BINARY_NAME status 2>&1 | jq .SyncInfo.catching_up${NC}"
